7974fb7bc8bc2b7591e4d9e0ef7476d519901644
Ichail <Ichail>
2023-04-20 23:49:11 +0300

    Addfourier func

---
+++ image_generation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_fourier(byte_string, numb_name) -> None:
+    pass_to_image_dir = "./images/"
+    x = np.frombuffer(byte_string, dtype=np.uint8)  # преобразуем байт-строку в числовой массив
+    fourier = np.fft.fft(x)  # вычисляем ряд Фурье
+    freq = np.fft.fftfreq(len(x))  # вычисляем дискретные частоты преобразования Фурье
+    plt.plot(freq, fourier)
+    plt.savefig(pass_to_image_dir + numb_name + ".png")
+    plt.close()
+
+
+def get_bytes_matrix(filename):
+    counter = 1
+    with open(filename, 'rb') as f:
+        while True:
+            data = f.read(100)
+            if not data:
+                break
+            f.seek(-99, 1)
+            counter += 1
+            print(data, end="\n\n")
+            get_fourier(data, str(counter))
+
+
+get_bytes_matrix(filename=)
